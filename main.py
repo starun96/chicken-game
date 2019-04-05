6bb11fa3f95008ca4c04483e8cddec8bb4793e83
starun96 <tarunsaharya@gmail.com>
2019-04-05 15:36:13 -0400

    fixed the spelling of "reaction_time"

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from scipy impoert stats
+from scipy import stats
 import numpy as np
 
 # ---------- Chicken Game ----------
@@ -10,7 +10,7 @@ MEMBERS = ["ts4pe", "jac9vn"]
 # avg would work, drawing from bothh parameters
 # estimate reaction time, estimate for m and slace, so play it at mean, so fine some point after
 
-global reacton_time   # the last reounds reactiont time
+global reaction_time   # the last reounds reactiont time
 global previous_move  # last rounds opponents swerve time
 global
 
@@ -18,14 +18,17 @@ outcome = -1
 
 swerve_time = 5
 
+
 prevSwerve_rounds = [1, 1, 3, 4, 2, 1]
 prevScore_rounds = [-1, -1, 1, 1, 1, -1]
+
 
 def regression_info(x, y):
     gradient, intercept, r_value, p_value, std_err = stats.linregress(x, y)
     print "Gradient and intercept:", gradient, intercept
     print "R-squared", r_value**2
     print "P-value", p_value
+
 
 def chose_next():
     if previous_move == 1:
@@ -36,10 +39,6 @@ def chose_next():
 
     elif previous_move == -10:
         return swerve_time + 1
-
-
-
-
 
 
 def get_move(state):
