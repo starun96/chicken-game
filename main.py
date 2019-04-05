9f365b10e8530e498f055a0226558a3bf9c1b896
Julian <juliancorn64@gmail.com>
2019-04-05 09:48:45 -0400

    upadtes py with notes from Freemore

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+
+
+# ---------- Chicken Game ----------
+
 TEAM_NAME = "tarjul"
 MEMBERS = ["ts4pe", "jac9vn"]
 
+# 100 repititions to chicken
+# avg would work, drawing from bothh parameters
+# estimate reaction time, estimate for m and slace, so play it at mean, so fine some point after mean
+
+global reacton_time   # the last reounds reactiont time
+global previous_move  # last rounds opponents swerve time
+global
 
 outcome = -1
 
@@ -26,3 +37,7 @@ def get_move(state):
     out = state.previous_rounds[len(state.previous_rounds) - 1]
     out = chose_next(out)
     return out
+
+# ---------- Connect More ----------
+
+# see if win is solvable (forced), means shorter connect value and thick board, else...
