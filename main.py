1a808a727593156ec8e485f8aa2334590983b634
Julian <juliancorn64@gmail.com>
2019-04-05 11:15:50 -0400

    added rough outline of line of regression code

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-
+from scipy impoert stats
+import numpy as np
 
 # ---------- Chicken Game ----------
 
@@ -7,7 +8,7 @@ MEMBERS = ["ts4pe", "jac9vn"]
 
 # 100 repititions to chicken
 # avg would work, drawing from bothh parameters
-# estimate reaction time, estimate for m and slace, so play it at mean, so fine some point after mean
+# estimate reaction time, estimate for m and slace, so play it at mean, so fine some point after
 
 global reacton_time   # the last reounds reactiont time
 global previous_move  # last rounds opponents swerve time
@@ -17,7 +18,14 @@ outcome = -1
 
 swerve_time = 5
 
-previous_rounds = []
+prevSwerve_rounds = [1, 1, 3, 4, 2, 1]
+prevScore_rounds = [-1, -1, 1, 1, 1, -1]
+
+def regression_info(x, y):
+    gradient, intercept, r_value, p_value, std_err = stats.linregress(x, y)
+    print "Gradient and intercept:", gradient, intercept
+    print "R-squared", r_value**2
+    print "P-value", p_value
 
 def chose_next():
     if previous_move == 1:
@@ -28,6 +36,7 @@ def chose_next():
 
     elif previous_move == -10:
         return swerve_time + 1
+
 
 
 
